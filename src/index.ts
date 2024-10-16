b3f66d0ae29883e3f2c6f42dc4d31bdc4717e844
eric-yates <hello@ericthecurious.com>
2024-10-16 15:26:43 -0600

    minor: create fake extractor

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { default as MangledNameExtractorImpl } from './MangledNameExtractor'
 export * from './MangledNameExtractor'
+
+export { default as FakeMangledNameExtractor } from './testDoubles/FakeMangledNameExtractor'
+export * from './testDoubles/FakeMangledNameExtractor'
---
+++ src/testDoubles/FakeMangledNameExtractor.ts
@@ -0,0 +1,32 @@
+import { MangledNameExtractor, MangledNameMap } from '../MangledNameExtractor'
+
+export default class FakeMangledNameExtractor implements MangledNameExtractor {
+    public static fakeResult: MangledNameMap = {}
+    public static extractCalls: FakeExtractCall[] = []
+
+    public async extract(libPath: string, unmangledNames: string[]) {
+        this.extractCalls.push({
+            libPath,
+            unmangledNames,
+        })
+        return this.fakeResult
+    }
+
+    private get fakeResult() {
+        return FakeMangledNameExtractor.fakeResult
+    }
+
+    private get extractCalls() {
+        return FakeMangledNameExtractor.extractCalls
+    }
+
+    public static clearTestDouble() {
+        FakeMangledNameExtractor.fakeResult = {}
+        FakeMangledNameExtractor.extractCalls = []
+    }
+}
+
+export interface FakeExtractCall {
+    libPath: string
+    unmangledNames: string[]
+}
