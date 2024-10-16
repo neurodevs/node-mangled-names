ff860e826697616fd8475cf52bc23796dcbfe82e
eric-yates <hello@ericthecurious.com>
2024-10-16 15:31:25 -0600

    patch: add numConstructorCalls to fake

--- src/testDoubles/FakeMangledNameExtractor.ts
+++ src/testDoubles/FakeMangledNameExtractor.ts
@@ -3,6 +3,11 @@ import { MangledNameExtractor, MangledNameMap } from '../MangledNameExtractor'
 export default class FakeMangledNameExtractor implements MangledNameExtractor {
     public static fakeResult: MangledNameMap = {}
     public static extractCalls: FakeExtractCall[] = []
+    public static numConstructorCalls = 0
+
+    public constructor() {
+        FakeMangledNameExtractor.numConstructorCalls++
+    }
 
     public async extract(libPath: string, unmangledNames: string[]) {
         this.extractCalls.push({
