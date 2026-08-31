d9e13b49f8c26530df13fd4987fb4f99ac84049c
eric-yates <hello@ericthecurious.com>
2026-08-31 10:28:36 -0700

    patch: fix lint

--- src/impl/MangledNameExtractor.ts
+++ src/impl/MangledNameExtractor.ts
@@ -1,5 +1,5 @@
-import { exec } from 'child_process'
-import { promisify } from 'util'
+import { exec } from 'node:child_process'
+import { promisify } from 'node:util'
 
 export default class MangledNameExtractor implements NameExtractor {
     public static Class?: NameExtractorConstructor
